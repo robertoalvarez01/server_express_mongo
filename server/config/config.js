611ffef457dd1053925dff62dc94020f77f5740f
Roberto Alvarez <robertogonzaloalvarez01@gmail.com>
2020-03-30 20:57:07 -0300

    Protect DB

--- server/config/config.js
+++ server/config/config.js
@@ -11,10 +11,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ============================
 //  DataBase
 // ============================
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost/cafe';
-// } else {
-urlDB = 'mongodb+srv://roberto_admin:eUzd1D4TMZxdG83a@cluster0-oqlsz.mongodb.net/cafe';
-// }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost/cafe';
+} else {
+    urlDB = process.env.MONGO_URL;
+}
 
 process.env.URLDB = urlDB;
